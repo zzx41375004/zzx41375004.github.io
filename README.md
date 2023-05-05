3910d8c8c4735f5076fb984ac5a6253ccca6bc67
zzx <309985004@qq.com>
2023-05-05 18:54:08 +0800

    完成猜数字小游戏

--- README.md
+++
@@ -1,6 +0,0 @@
-# 我的网站demo
-
-## Reference
-
-[mdn](https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/Publishing_your_website)
-
--- scripts/main.js
+++ scripts/main.js
@@ -22,7 +22,7 @@ function setUserName() {
 		setUserName();
 	} else {
 		localStorage.setItem("name", myName);
-		myHeading.innerHTML = "Mozilla is cool, " + myName;
+		myHeading.innerHTML = "你真是太酷了, " + myName;
 	}
 }
 
@@ -30,7 +30,7 @@ if (!localStorage.getItem("name")) {
 	setUserName();
 } else {
 	let storedName = localStorage.getItem("name");
-	myHeading.innerHTML = "Mozilla is cool, " + storedName;
+	myHeading.innerHTML = "你真是太酷了, " + storedName;
 }
 
 myButton.onclick = function () {
